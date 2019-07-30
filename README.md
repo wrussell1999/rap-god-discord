31de4452b7f34f4484bfac03930163ce93df57c8
Will Russell <jetpowerss@gmail.com>
2019-07-30 23:09:14 +0100

    Basic bot structure

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Rap God - Discord Edition
 
 [Rap God](https://github.com/jedevc/royal-hackaway-2019) was a pretty cool hackathon project, built using the Nexmo API. I thought it would be cool to implement the same thing, but inside of Discord using voice channels...
+
+## Development
+
+```bash
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip3 install -r requirements.txt
+```
--- rapgod/rapgod.py
+++ rapgod/rapgod.py
@@ -1,2 +1,30 @@
+import discord
+from discord.ext import commands
+from . import text_to_mp3
+from . import lyrics
+import threading
+from collections import deque
+import uuid
+import logging
+
+bot = commands.Bot(command_prefix='!')
+
 def main():
-    pass
+    logging.basicConfig(level=logging.INFO)
+
+@bot.event
+async def on_ready():
+    print('Ready!')
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    await bot.process_commands(message)
+
+@bot.command()
+async def rap(ctx):
+    try:
+        word = ctx.message.content.split(' ')[0]
+    except:
+        ctx.channel.send('Not valid')
