23dab73ceb01538deb380762ec22cc7bf8a72a00
Will Russell <jetpowerss@gmail.com>
2019-07-30 22:48:15 +0100

    Initial commit
    Initial code from rap god project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rap God - Discord Edition
+
+[Rap God](https://github.com/jedevc/royal-hackaway-2019) was a pretty cool hackathon project, built using the Nexmo API. I thought it would be cool to implement the same thing, but inside of Discord using voice channels...
---
+++ rapgod/text_to_mp3.py
@@ -0,0 +1,34 @@
+import os
+from google.cloud import texttospeech
+from pydub import AudioSegment
+
+client = texttospeech.TextToSpeechClient()
+
+
+def make_mp3(text, filename):
+    print("Starting sound synthesis...")
+    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3,
+        speaking_rate=0.93,
+        pitch=-6.0)
+
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    with open(filename, 'w+b') as out:
+        out.write(response.audio_content)
+        print(f'Audio content written to {filename}')
+    sound1 = AudioSegment.from_mp3(filename)
+    sound2 = AudioSegment.from_mp3("supreme.mp3")
+    output = sound1.overlay(sound2, position=100)
+
+    new_filename = os.path.join(os.path.dirname(
+        filename), 'mod-' + os.path.basename(filename))
+    print(new_filename)
+    output.export(new_filename, format="mp3")
+    return new_filename
