a83d3903b8670cff2090c25c6f41d610ba94a273
Daniel Spencer <danielfspencer1@gmail.com>
2019-08-27 16:22:49 +0100

    Support multiple servers with 'use' comamnd

---
+++ rapgod/config.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import json
+
+DISCORD_CONFIG_PATH = 'config/discord.json'
+VOICE_CHANNEL_CONFIG_PATH = 'config/voice_channel_map.json'
+
+
+def load_config():
+    global token, command_prefix, thread_count, voice_channel_map
+
+    print('Loading config files...')
+    try:
+        with open(DISCORD_CONFIG_PATH) as file:
+            try:
+                config = json.load(file)
+            except json.decoder.JSONDecodeError:
+                die(f'Cannot parse {DISCORD_CONFIG_PATH}')
+
+            try:
+                token = config['token']
+                command_prefix = config['command_prefix']
+                thread_count = config['thread_count']
+            except KeyError:
+                fatal(f'Cannot find required keys in {DISCORD_CONFIG_PATH}\
+                      \nSee README.md for required structure')
+    except OSError:
+        fatal('Cannot open discord.json config file')
+
+    try:
+        with open(VOICE_CHANNEL_CONFIG_PATH) as file:
+            print('Using existing voice_channel_map')
+            try:
+                voice_channel_map = json.load(file)
+            except json.decoder.JSONDecodeError:
+                fatal(f'Cannot parse {VOICE_CHANNEL_CONFIG_PATH}\
+                      \nTry deleting the file')
+    except OSError:
+        print('Creating empty voice_channel_map')
+        voice_channel_map = {}
+
+
+def save_voice_channel_map():
+    try:
+        with open(VOICE_CHANNEL_CONFIG_PATH, 'w') as file:
+            json.dump(voice_channel_map, file)
+            print('Saved voice_channel_map')
+    except OSError:
+        print(f'Cannot save to {VOICE_CHANNEL_CONFIG_PATH}')
+
+
+def fatal(message):
+    print(message)
+    sys.exit(1)
--- rapgod/rapgod.py
+++ rapgod/rapgod.py
@@ -1,33 +1,32 @@
+import io
 import json
 import logging
 import asyncio
 import discord
-from io import BytesIO
 
 from discord.ext import commands
 
 from queue import Empty
 
 from . import worker
+from rapgod import config
 
-bot = commands.Bot(command_prefix='!')
-pool = worker.ContainedPool(thread_count=4)
+config.load_config()
+
+pool = worker.ContainedPool(thread_count=config.thread_count)
+bot = commands.Bot(command_prefix=config.command_prefix)
 last_song_cache = {}
-
-with open('config/discord.json') as file:
-    config = json.load(file)
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    token = config['token']
     pool.start()
-    bot.run(token)
-
+    bot.run(config.token)
+    pool.stop()
 
 @bot.event
 async def on_ready():
-    print('Starting response dispatcher')
+    print('Starting response dispatcher...')
     bot.loop.create_task(response_dispatcher())
     print('Bot ready!')
 
@@ -48,11 +47,23 @@ async def rap(ctx):
     theme_words = ' '.join(words[1:])
 
     if ctx.message.channel.type == discord.ChannelType.private:
+        # For DM channels we just send the mp3 file because we can't call them
         print(f'- Enqueue \'make_and_encode\' (theme \'{theme_words}\')')
         pool.enqueue('make_and_encode', (theme_words, ctx.message.channel.id))
     else:
+        # For server channels we play in the vocie channel
         print(f'- Enqueue \'make_track\' (theme \'{theme_words}\')')
-        pool.enqueue('make_track', (theme_words, config['voice_channel_id']))
+
+        try:
+            server_id_string = str(ctx.guild.id)
+            voice_channel_id = config.voice_channel_map[server_id_string]
+        except KeyError:
+            await ctx.send('I don\'t know which voice channel to rap in.\n'
+                           'A server admin should run the command `!set (name'
+                           'of voice channel)` to fix this')
+            return
+
+        pool.enqueue('make_track', (theme_words, voice_channel_id))
 
     await ctx.trigger_typing()
 
@@ -66,6 +77,41 @@ async def save(ctx):
         await ctx.trigger_typing()
     except KeyError:
         await ctx.send('No previous song found')
+
+
+@bot.command()
+async def use(ctx):
+    user_permissions = ctx.channel.permissions_for(ctx.message.author)
+
+    if not user_permissions.administrator:
+        await ctx.send('Only server admins can configure the voice channel')
+        return
+
+    words = ctx.message.content.split(' ')
+
+    if len(words) != 2:
+        await ctx.send('Usage: `!set (voice channel name)`')
+        return
+
+    channel_name = words[1]
+
+    channel_object = discord.utils.get(ctx.guild.voice_channels,
+                                       name=channel_name)
+
+    if channel_object is None:
+        await ctx.send(f'No voice channel with name \'{channel_name}\' '
+                       'found')
+        return
+
+    server_id_string = str(ctx.guild.id)
+    config.voice_channel_map[server_id_string] = channel_object.id
+    config.save_voice_channel_map()
+
+    await ctx.send(f'Voice channel \'{channel_name}\' (id: '
+                   f'{channel_object.id}) will be used')
+
+    print(f'Server \'{server_id_string}\' associated voice channel is now '
+          f'\'{channel_object.id}\'')
 
 
 async def response_dispatcher():
@@ -94,7 +140,7 @@ async def play_audio(stream, channel_id):
     voice_channel = bot.get_channel(channel_id)
     print(f'- Dispatch audio to \'{voice_channel}\'')
 
-    last_song_cache[str(channel_id)] = BytesIO(stream.getvalue())
+    last_song_cache[str(channel_id)] = io.BytesIO(stream.getvalue())
 
     try:
         voice_client = await voice_channel.connect()
